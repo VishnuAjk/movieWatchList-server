a0aa22ee7dac1b2adb5cbfc5ba0705e6baa7c831
VISHNU <145429961+VishnuAjk@users.noreply.github.com>
2024-06-27 11:48:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ import movieRouter from "./src/Routes/movie.routes.js";
 import { connectToDatabase } from "./src/Config/mongoose.js";
 import jwtAuth from "./src/Middlewares/jwtAuth.middleware.js";
 const app = express();
-app.use(cors({
-    origin: ["https://tangerine-tarsier-b4ba77.netlify.app/"],
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 
